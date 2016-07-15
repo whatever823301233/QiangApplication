b5a78094c3386f0cc183919a1e85abb6aed2fa01
whatever823301233 <xq823301233@sina.com>
2016-07-15 15:49:48 +0800

    volley

--- app/src/main/java/com/lovedou/qiang/qiangapplication/activity/MainActivity.java
+++ app/src/main/java/com/lovedou/qiang/qiangapplication/activity/MainActivity.java
@@ -1,15 +1,37 @@
 package com.lovedou.qiang.qiangapplication.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.lovedou.qiang.qiangapplication.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ActivityBase {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RequestQueue mQueue = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest("http://www.baidu.com",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("TAG", response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("TAG", error.getMessage(), error);
+            }
+        });
+
+        mQueue.add(stringRequest);
+
     }
 }
